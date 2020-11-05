ffccdb6611f83e5129e05d147aedd4e0ff289591
checho1998 <c.hecho.28@hotmail.com>
2020-11-05 15:37:12 -0500

    fin

--- src/main/java/eci/ieti/controller/RESTController.java
+++ src/main/java/eci/ieti/controller/RESTController.java
@@ -11,6 +11,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.http.MediaType;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Criteria;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,8 +34,19 @@ public class RESTController {
     @RequestMapping("/files/{filename}")
     public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
 
-        //TODO implement method
-        return null;
+        try {
+            GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+            if (file != null) {
+                GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+                return ResponseEntity.ok().contentType(MediaType.valueOf(resource.getContentType()))
+                        .body(new InputStreamResource(resource.getInputStream()));
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
